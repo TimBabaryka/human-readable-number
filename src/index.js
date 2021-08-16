0e9f2b1454ca35db124090dd736cfc5e74cc9091
Tim Babaryka <timasky95@gmail.com>
2021-08-16 23:38:08 +0300

    Feat: new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    var arr1 = ['zero','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve', 'thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    var arr2 = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    
+    if (number < 20) {
+       let word = arr1[number];
+       return word;
+    }
 }
+
+
+
+
